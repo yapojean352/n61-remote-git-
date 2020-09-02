5eaaf2105c4412c2200e36be1530d3f9ebd3f9c3
yapojean352 <jeanreneyapo@yahoo.com>
2020-09-02 14:49:52 -0400

    Demarrer:ajout des trois fichier au remote

---
+++ main.js
@@ -0,0 +1,6 @@
+"use strict";
+
+var content = 'January February March April May June July August September October November December'.split(' ');
+var weekDayName = 'SUN MON TUES WED THURS FRI SAT'.split(' ');
+var daysOfMonth = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
+let Day = ['lundi','Mardi','Mercredi','Jeudi','Vendredi','samedi','Dimanche'];
